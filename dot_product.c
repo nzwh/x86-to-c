82ea87ccdb4079829a40c64fb04f7c60bfa25279
jms <74128476+jmslouis@users.noreply.github.com>
2024-04-01 15:03:48 +0800

    Add value randomizer

--- dot_product.c
+++ dot_product.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <conio.h>
 
 const int n20 = 1048576;    // 2^20
 const int n24 = 16777216;   // 2^24
@@ -19,6 +20,8 @@ double dp_formula(int size, double *vector_a, double *vector_b) {
 double runtime_c(double (*execution)(int, double *, double *), int size, double *vector_a, double *vector_b) {
   clock_t start = clock();
   double dot_product = execution(size, vector_a, vector_b);
+  //printf("Press any key to continue...\n");
+  //getchar();
   clock_t end = clock();
 
   double time = ((double)(end - start)) / CLOCKS_PER_SEC;
@@ -26,7 +29,7 @@ double runtime_c(double (*execution)(int, double *, double *), int size, double 
 }
 
 int main() {
-  srand(time(NULL));
+  srand((unsigned int)time(NULL));
 
   int size;
   printf("Input a vector size: ");
@@ -41,21 +44,25 @@ int main() {
   double time_asm = 0.0;
   double dot_product = 0;
 
-  for (int i = 0; i < size; i++) {
-    vector_a[i] = 2.0;
-    vector_b[i] = 4.0;
-  }
-
   for (int i = 0; i < runs; i++) {
+    for (int j = 0; j < size; j++) {
+        vector_a[j] = (double)rand() / RAND_MAX;
+        vector_b[j] = (double)rand() / RAND_MAX;
+    }
     double time_result_c = runtime_c(dp_formula, size, vector_a, vector_b);
     dot_product = dp_formula(size, vector_a, vector_b);
+
+    printf("Dot product in C: %.2f\n", dot_product);
+    printf("Runtime %d: %.32f seconds\n", i+1, time_result_c);
     
     time_c += time_result_c;
   }
 
   double average_time_c = time_c / runs;
-  printf("Dot product in C: %.2f\n", dot_product);
   printf("Average execution time in C: %.6f seconds\n", average_time_c);
+
+  free(vector_a);
+  free(vector_b);
 
   return 0;
 }
